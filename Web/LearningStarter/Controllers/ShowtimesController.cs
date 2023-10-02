669d19d1943325a302fdd7571aee586ec8ee27b6
Satyam Pathak <satyampathak8059@gmail.com>
2023-10-01 23:41:34 -0500

    Updated Showtimes Controller

--- Web/LearningStarter/Controllers/ShowtimesController.cs
+++ Web/LearningStarter/Controllers/ShowtimesController.cs
@@ -1,5 +1,5 @@
 ﻿using System.Linq;
-
+using System.Net.Http.Headers;
 using LearningStarter.Common;
 using LearningStarter.Data;
 using LearningStarter.Entities;
@@ -49,7 +49,25 @@ public class ShowtimesController : ControllerBase
 	{
 		var response = new Response();
 
-		var showtimeToCreate = new Showtimes
+		if (createDto.Price < 0)
+		{
+			response.AddError(nameof(createDto.Price), "Price must be positive");
+		}
+        if (createDto.AvailableSeats < 0)
+        {
+            response.AddError(nameof(createDto.AvailableSeats), "AvailableSeats must be positive");
+        }
+
+        if (string.IsNullOrEmpty(createDto.Screen))
+        {
+            response.AddError(nameof(createDto.Screen), "Screen name is required");
+
+        }
+		if (response.HasErrors)
+		{
+			return BadRequest(response);
+		}
+        var showtimeToCreate = new Showtimes
 		{
 			MovieId = createDto.MovieId,
 			StartTime = createDto.StartTime,
@@ -77,4 +95,78 @@ public class ShowtimesController : ControllerBase
 
 		return Created("", response);
 	}
+	[HttpPut("{id}")]
+	public IActionResult Update([FromBody] ShowtimesUpdateDto updateDto,int id)
+	{
+		var response = new Response();
+
+        if (updateDto.Price < 0)
+        {
+            response.AddError(nameof(updateDto.Price), "Price must be positive");
+        }
+        if (updateDto.AvailableSeats < 0)
+        {
+            response.AddError(nameof(updateDto.AvailableSeats), "AvailableSeats must be positive");
+        }
+
+        if (string.IsNullOrEmpty(updateDto.Screen))
+        {
+            response.AddError(nameof(updateDto.Screen), "Screen name is required");
+        }
+        var showtimestoUpdate = _dataContext.Set<Showtimes>()
+			.FirstOrDefault(showtimes => showtimes.Id == id);
+        if (showtimestoUpdate == null)
+        {
+            response.AddError("id", "showtimes not found.");
+
+        }
+        if (response.HasErrors)
+        {
+            return BadRequest(response);
+
+        }
+        showtimestoUpdate.StartTime = updateDto.StartTime;
+		showtimestoUpdate.TheaterID = updateDto.TheaterID;
+		showtimestoUpdate.Price = updateDto.Price;
+		showtimestoUpdate.AvailableSeats = updateDto.AvailableSeats;
+		showtimestoUpdate.Screen = updateDto.Screen;
+		_dataContext.SaveChanges();
+		var showtimetoReturn = new ShowtimesGetDto
+		{
+			Id = showtimestoUpdate.Id,
+			MovieId = showtimestoUpdate.MovieId,
+			StartTime = showtimestoUpdate.StartTime,
+			TheaterID = showtimestoUpdate.TheaterID,
+
+		};
+		response.Data = showtimetoReturn;
+		return Ok(response);
+}
+	[HttpDelete("{id}")]
+	public IActionResult Delete(int id)
+	{
+        var response = new Response();
+		var showtimestoDelete = _dataContext.Set<Showtimes>()
+			.FirstOrDefault(showtimes => showtimes.Id == id);
+		if(showtimestoDelete==null)
+		{
+			response.AddError("id", "showtimes not found.");
+
+		}
+		if (response.HasErrors)
+		{
+			return BadRequest(response);
+
+		}
+		_dataContext.Set<Showtimes>().Remove(showtimestoDelete);
+		_dataContext.SaveChanges();
+
+		response.Data = true;
+		return Ok(response);
+			
+        }
+
+    
+
+
 }
