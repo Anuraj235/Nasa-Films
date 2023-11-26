a7e346a346df9fc3603032f846b82acc9820ac2d
Anuraj235 <pantanuraj31@gmail.com>
2023-11-26 16:49:21 -0600

    commented database auto delete

--- Web/LearningStarter/Startup.cs
+++ Web/LearningStarter/Startup.cs
@@ -107,7 +107,7 @@ public class Startup
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env, DataContext dataContext, ILogger<Startup> logger)
     {
-        dataContext.Database.EnsureDeleted();
+        //dataContext.Database.EnsureDeleted();
         dataContext.Database.EnsureCreated();
 
         app.UseHsts();
